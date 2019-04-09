e8b33586c6ea9a1387072e4958d05949b6ed61d3
Kah Hau Yap <kahhauyap@gmail.com>
2019-04-09 17:37:55 -0400

    Configure login and registration routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,21 +4,21 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
-
+const secretKey = "banana";
 router.post("/register", (req, res) => {
-    //Check the database to see if the email already exists
+    // Check the database to see if the email already exists
     User.findOne({ email: req.body.email }).then(user => {
         if (user) {
             return res.status(400).json({ email: "Email already exists" });
         }
-        //If email doesn't exist then create a new user
+        // If email doesn't exist then create a new user
         const newUser = new User({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
         });
 
-        //Hash password before saving
+        // Hash password before saving
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
                 if (err) throw err;
@@ -31,5 +31,40 @@ router.post("/register", (req, res) => {
         });
     });
 });
+
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            return res.status(404).json({ emailnotfound: "Email not found" });
+        }
+        // Compare the password with hash
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                // User matched so create payload
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+                // Sign jwt token
+                jwt.sign(payload, secretKey,
+                    { expiresIn: 31556926 },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else { // Password didn't match so send error
+                return res.status(400).json({ passwordincorrect: "Password incorrect" });
+            }
+        });
+    });
+});
+module.exports = router;
 
 
--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const passport = require("passport");
+
+const users = require("./routes/users");
 
 const app = express();
 
-//Bodyparser middleware
+// Bodyparser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//mLab MongoDB database
+// mLab MongoDB database
 const mongoDB = 'mongodb://mongodb:password1@ds125673.mlab.com:25673/stocks';
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
-//Set port to 3001 if no other port specified
-const port = process.env.PORT || 3001;
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./passport")(passport);
+
+// Routes
+app.use("/users", users);
+
+// Set port to 3001 if no other port specified
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
